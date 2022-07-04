58dfb82c9ff5e2e72e2260c146f06670457b27d2
shubham-allprojects <parab16aug@gmail.com>
2022-07-04 14:31:20 +0530

    sws

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const mongose = require("mongoose");
 
 const dbconnection = () => {
   mongose
-    .connect(process.env.MONGO, {
+    .connect('mongodb://alms-mongodb:LLXgzHFTLDumql8vQgUzBFm6RhRGL4q7NCnTNdcxOAzhxNHxVqwlMA7H9aoWsHPAiErjt0vSFAgL9gqTHi4nDg==@alms-mongodb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@alms-mongodb@', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
